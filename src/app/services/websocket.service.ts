fcaa995110afaecd138b644d5847ca633e8a3cb8
Brandon Beach <brandon.beach23@gmail.com>
2026-04-19 22:45:06 -0400

    Implemented disconnect(), sendMessage(), and subscribe() in websocket.service.

--- src/app/services/websocket.service.ts
+++ src/app/services/websocket.service.ts
@@ -14,35 +14,43 @@ export class WebsocketService {
   private messageSubject = new Subject<string>();
   public message$ = this.messageSubject.asObservable();
   
-  private stompClient = null;
+  private stompClient: any = null;
 
   constructor() { }
 
   connect(){
-        const socket = new SockJS('http://localhost:8080/ws/timer');
-        this.stompClient = Stomp.over(socket);
+    const socket = new SockJS('http://localhost:8080/ws/timer');
+    this.stompClient = Stomp.over(socket);
 
-        this.stompClient.connect({}, (frame) => {
-            this.statusSubject.next('Connected');
-            this.stompClient.subscribe('/topic/test', (message) => {
-                this.messageSubject.next(JSON.parse(message.body));
-            });
-        }, (error) => {
-            this.statusSubject.next('Error' + error.message);
-        });
+    this.stompClient!.connect({}, (frame: any) => {
+      this.statusSubject.next('Connected');
+      this.stompClient!.subscribe('/topic/test', (message: any) => {
+        this.messageSubject.next(JSON.parse(message.body));
+      });
+      }, (error: any) => {
+          this.statusSubject.next('Error' + error.message);
+      });
   }
 
   disconnect(): void {
-
+    if(this.stompClient){
+      this.stompClient.disconnect(() => {
+        this.statusSubject.next('Disconnected');
+      });      
+    }
   }
 
   sendMessage(destination: string, body: any): void {
-
+    if (this.stompClient){
+      this.stompClient.send(destination, {}, JSON.stringify(body));
+    }
   }
 
   subscribe(destination: string): void {
-
+    if (this.stompClient){
+      this.stompClient.subscribe(destination, (message: any) => {
+        this.messageSubject.next(JSON.parse(message.body));
+      });
+    }
   }
-
-
 }
